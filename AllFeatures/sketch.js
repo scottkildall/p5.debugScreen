6a0fe93e8f137fc26080092fdc86d11fab63eda9
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2021-03-22 19:15:20 -0700

    sketch.js for AllFeatures example

--- AllFeatures/sketch.js
+++ AllFeatures/sketch.js
@@ -1,9 +1,9 @@
 /***********************************************************************************
-  SimpleDebug
+  AllFeatures
   by Scott Kildall
 
   Uses the p5.debugScreen.js library to allocate and output simple mouse-clicked
-  messages on the screen.
+  messages on the screen. Shows all accessors
 
 ------------------------------------------------------------------------------------
 	To use:
@@ -12,11 +12,14 @@
   <script src="p5.debugScreen.js"></script>
 ***********************************************************************************/
 
-// Make global 
+// global var for the debug screen object
 var debugScreen;
+var lineCount;      // # of lines for the screen
+
+// drawing vars
 var yTextPos = 30;
 var lineHeight = 18;
-var lineCount;
+
 
 // Setup code goes here
 function setup() {
@@ -34,8 +37,8 @@ function setup() {
 function draw() {
   background(128);
 
+  // draw text instuctions
   fill(255);
-
   textAlign(LEFT)
   let textX = width/2;
   text( "Click on mouse to add debug text", textX, yTextPos);  
@@ -50,63 +53,72 @@ function draw() {
   text( "Press UP ARROW to increase font size", textX, yTextPos + lineHeight*9);
   text( "Press DOWN ARROW to decrease font size", textX, yTextPos + lineHeight*10);
 
+  // draw debug srreen
   debugScreen.draw();
 }
 
 function keyPressed() {
+  // draw from top
   if( key === 't' ) {
     debugScreen.drawFromTop();
     debugScreen.print("setting to draw from top");
     return;
   }
 
+  // draw from bottom
   if( key === 'b' ) {
     debugScreen.drawFromBottom();
     debugScreen.print("setting to draw from bottom");
-
   }
 
+  // change to red text
   if( key === 'r') {
     debugScreen.setTextColor(color(255,0,0));
   }
 
-
+  // back to green text
   if( key === 'g') {
     debugScreen.setTextColor(color(255,0,0));
   }
 
+  // hide background rect
   if( key === 'x') {
     debugScreen.setDrawBackgroundRect(false);
   }
 
+  // show background rect
   if( key === 'y') {
     debugScreen.setDrawBackgroundRect(true);
     return;
   }
 
+  // turn on autoscroll
   if( key === 'a' ) {
-    debugScreen.setAutoScroll(false);
-    return;
-  }
-
-  if( key === 'm' ) {
     debugScreen.setAutoScroll(true);
     return;
   }
-  
 
+  // turn off autoscroll
+  if( key === 'm' ) {
+    debugScreen.setAutoScroll(false);
+    return;
+  }
+  
+  // increase font size
   if (keyCode === UP_ARROW) {
     lineCount++;
     debugScreen.setLineCount(lineCount);
     debugScreen.setTextSize(debugScreen.getTextSize()+1);
   }
 
-  if (keyCode ===DOWN_ARROW) {
+  // decrease font size
+  if (keyCode === DOWN_ARROW) {
     lineCount--;
     debugScreen.setLineCount(lineCount);
     debugScreen.setTextSize(debugScreen.getTextSize()-1);
   }
 
+  // if wr are on autoscroll, output to specific line
   if( debugScreen.getAutoScroll() === false ) {
     // output to line #2
     debugScreen.print("keyPressed | keyPressed = " + key, 1 );
